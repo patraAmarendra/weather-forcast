ca479a0c02ba22fbd94cbf22155be796548193c9
patraAmarendra <amarendrapatra14@gmail.com>
2024-10-24 09:40:56 +0530

    weather init

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="sticky top-0 bg-primary shadow-lg">
+    <nav
+      class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+    >
+      <RoterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">Weather Forcast</p>
+        </div>
+      </RoterLink>
+      <div class="flex gap-4 flex-1 justify-end">
+        <i
+          class="fa-solid fa-circle-info text-xl hover:text-secondary duration-150 cursor-pointer"
+        ></i>
+        <i
+          class="fa-solid fa-plus text-xl hover:text-secondary duration-150 cursor-pointer"
+        ></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<style lang="scss" scoped>
+</style>
